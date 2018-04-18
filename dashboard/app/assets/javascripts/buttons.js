e3dbe0c0266af8328c3705a835d2acfcc1f47a54
Calum <cjmcguicken@gmail.com>
2018-04-18 16:47:58 +0100

    Button flip on view change

--- dashboard/app/assets/javascripts/buttons.js
+++ dashboard/app/assets/javascripts/buttons.js
@@ -1,10 +1,22 @@
 $(document).ready(function(){
+
     $('#download_data').click(function(){
 	  $("#temperatureChart").get(0).toBlob(function(blob) {
 	    saveAs(blob, "temperatureChart");
 	  });
     });
+
+    $('#save_image').click(function(){
+	  $("#temperatureChart").get(0).toBlob(function(blob) {
+	    saveAs(blob, "temperatureChart");
+	  });
+    });
+
     $('#switch_view').click(function(){
-        $('.panel').toggleClass('flipped');
+		$(this).text(function(i, text){
+			return text === "View Graph" ? "View Data" : "View Graph";
+		})
+        $('.panel1').toggleClass('flipped');
+        $('.panel2').toggleClass('flipped');
     });
 });
