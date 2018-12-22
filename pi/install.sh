593939d339d1f3a0741e987e2704ee889bdca737
Calum <cjmcguicken@gmail.com>
2018-12-22 14:18:25 +0000

    Removed whitespace bash script

--- pi/install.sh
+++ pi/install.sh
@@ -3,27 +3,21 @@
 #Check for updates
 echo "Checking for updates....."
 sudo apt-get update
-
 #Install essential python libraries
 echo "Installing essential Python libraries....."
 sudo apt-get install build-essential python-dev python-openssl git
-
 #Cloning Adafruit Python Libraries
 echo "Cloning Adafruit Python Libraries....."
 git clone https://github.com/adafruit/Adafruit_Python_DHT.git && cd Adafruit_Python_DHT
-
 #Install Adafruit library for Python 2 & 3
 echo "Installing essential Python libraries....."
 sudo python setup.py install
 sudo python3 setup.py install
-
 #Get DHT sensor type as variable
 echo "What DHT Sensor? Please type 11/22, followed by [ENTER]:"
 read sensorVersion
-
 #Get GPIO number as variable
 echo "What GPIO number is the sensor connected to? Followed by [ENTER]:"
 read gpioNumber
-
 cd examples
 sudo ./AdafruitDHT.py sensorVersion gpioNumber
