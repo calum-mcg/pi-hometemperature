87baadc1e9228592e0f1b62aa929f0c31439d64d
Calum <cjmcguicken@gmail.com>
2018-12-23 17:40:37 +0000

    Added rails pi installation

--- pi/install.sh
+++ pi/install.sh
@@ -7,6 +7,15 @@ sudo apt-get update
 echo "\nInstalling essential Python libraries (Python 2 and 3)....."
 sudo apt-get install build-essential python-dev python-openssl git
 sudo apt-get install python3 python3-dev python3-setuptools
+#Install rails
+echo "\nInstalling requirements for rails....."
+sudo apt-get install -y git curl zlib1g-dev subversion
+sudo apt-get install -y openssl libreadline6-dev git-core zlib1g libssl-dev
+sudo apt-get install -y libyaml-dev libsqlite3-dev sqlite3
+sudo apt-get install -y libxml2-dev libxslt-dev
+sudo apt-get install -y autoconf automake libtool bison
+curl -L get.rvm.io | bash -s stable --rails
+source ~/.rvm/scripts/rvm
 #Cloning Adafruit Python Libraries
 echo "\nCloning Adafruit Python Libraries....."
 git clone https://github.com/adafruit/Adafruit_Python_DHT.git
