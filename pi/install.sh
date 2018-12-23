154fff917331ba59c3dffe315efef7895ced4032
Calum <cjmcguicken@gmail.com>
2018-12-23 17:33:18 +0000

    Updated echo outputs

--- pi/install.sh
+++ pi/install.sh
@@ -1,25 +1,27 @@
 #!/usr/bin/env bash
 
 #Check for updates
-echo "Checking for updates....."
+echo "\nChecking for updates....."
 sudo apt-get update
 #Install essential python libraries
-echo "Installing essential Python libraries (Python 2 and 3)....."
+echo "\nInstalling essential Python libraries (Python 2 and 3)....."
 sudo apt-get install build-essential python-dev python-openssl git
 sudo apt-get install python3 python3-dev python3-setuptools
 #Cloning Adafruit Python Libraries
-echo "Cloning Adafruit Python Libraries....."
+echo "\nCloning Adafruit Python Libraries....."
 git clone https://github.com/adafruit/Adafruit_Python_DHT.git
 cd Adafruit_Python_DHT
 #Install Adafruit library for Python 2 & 3
-echo "Installing essential Python libraries....."
+echo "\nInstalling essential Python libraries....."
 sudo python setup.py install
 sudo python3 setup.py install
 #Get DHT sensor type as variable
-echo "What DHT Sensor? Please type 11/22, followed by [ENTER]:"
+echo "\nWhat DHT Sensor? Please type 11/22, followed by [ENTER]:"
 read sensorVersion
 #Get GPIO number as variable
-echo "What GPIO number is the sensor connected to? Followed by [ENTER]:"
+echo "\nWhat GPIO number is the sensor connected to? Followed by [ENTER]:"
 read gpioNumber
+#Test sensor
+echo "\nTesting sensor....."
 cd examples
 sudo ./AdafruitDHT.py $sensorVersion $gpioNumber
