b5c3a866dbcd974adf9e5b1ede07979609bbcf9b
Calum <cjmcguicken@gmail.com>
2018-12-23 12:46:14 +0000

    Added python file to log temp and humidity

--- pi/install.sh
+++ pi/install.sh
@@ -4,11 +4,13 @@
 echo "Checking for updates....."
 sudo apt-get update
 #Install essential python libraries
-echo "Installing essential Python libraries....."
+echo "Installing essential Python libraries (Python 2 and 3)....."
 sudo apt-get install build-essential python-dev python-openssl git
+sudo apt-get install python3 python3-dev python3-setuptools
 #Cloning Adafruit Python Libraries
 echo "Cloning Adafruit Python Libraries....."
-git clone https://github.com/adafruit/Adafruit_Python_DHT.git && cd Adafruit_Python_DHT
+git clone https://github.com/adafruit/Adafruit_Python_DHT.git
+cd Adafruit_Python_DHT
 #Install Adafruit library for Python 2 & 3
 echo "Installing essential Python libraries....."
 sudo python setup.py install
@@ -20,4 +22,4 @@ read sensorVersion
 echo "What GPIO number is the sensor connected to? Followed by [ENTER]:"
 read gpioNumber
 cd examples
-sudo ./AdafruitDHT.py sensorVersion gpioNumber
+sudo ./AdafruitDHT.py $sensorVersion $gpioNumber
