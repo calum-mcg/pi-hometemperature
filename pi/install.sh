28712799b3a9d6be13f5f7250927f6a8644d8cb5
Calum <cjmcguicken@gmail.com>
2018-12-27 23:24:12 +0000

    Updated rails installation in bash script

--- pi/install.sh
+++ pi/install.sh
@@ -6,15 +6,6 @@ sudo apt-get update
 printf "\nInstalling essential Python libraries (Python 2 and 3)....."
 sudo apt-get install build-essential python-dev python-openssl git
 sudo apt-get install python3 python3-dev python3-setuptools
-#Install rails
-printf "\nInstalling requirements for rails....."
-sudo apt-get install -y git curl zlib1g-dev subversion
-sudo apt-get install -y openssl libreadline6-dev git-core zlib1g libssl-dev
-sudo apt-get install -y libyaml-dev libsqlite3-dev sqlite3
-sudo apt-get install -y libxml2-dev libxslt-dev
-sudo apt-get install -y autoconf automake libtool bison
-curl -L get.rvm.io | bash -s stable --rails
-source ~/.rvm/scripts/rvm
 #Cloning Adafruit Python Libraries
 printf "\nCloning Adafruit Python Libraries....."
 git clone https://github.com/adafruit/Adafruit_Python_DHT.git
@@ -46,7 +37,7 @@ sudo chmod -R 755 ../Temperature_Logging
 #Get Python script from github
 wget https://raw.githubusercontent.com/emperorcal/pi-hometemperature/master/pi/log.py
 sudo python3 /home/pi/Temperature_Logging/log.py $sensorVersion $gpioNumber
-#Create cronjob
+#Create cronjobs
 printf "\nCreating cronjob....."
 sudo crontab -l > mycron
 #echo new cron into cron file
@@ -54,3 +45,17 @@ sudo echo "* * * * * sudo python3 /home/pi/Temperature_Logging/log.py $sensorVer
 #install new cron file
 sudo crontab mycron
 sudo rm mycron
+#Install rails
+printf "\nInstalling requirements for rails....."
+cd /home/pi/
+sudo apt-get install -y git curl zlib1g-dev subversion
+sudo apt-get install -y openssl libreadline6-dev git-core zlib1g libssl-dev
+sudo apt-get install -y libyaml-dev libsqlite3-dev sqlite3
+sudo apt-get install -y libxml2-dev libxslt-dev
+sudo apt-get install -y autoconf automake libtool bison
+sudo curl -L get.rvm.io | bash -s stable --rails
+#If this fails you may need to use the gpg2 --keyserver command given as an error message to update the signatures
+sudo source ~/.rvm/scripts/rvm
+#Copy rails project over from Github
+cd Temperature_Logging
+sudo svn checkout https://github.com/emperorcal/pi-hometemperature/trunk/dashboard
