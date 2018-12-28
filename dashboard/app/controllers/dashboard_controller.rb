bb4880bfef001891d689aeccf1deb838932ecc4f
Calum <cjmcguicken@gmail.com>
2018-12-28 16:07:20 +0000

    Updated controller to accomodate humidity

--- dashboard/app/controllers/dashboard_controller.rb
+++ dashboard/app/controllers/dashboard_controller.rb
@@ -1,7 +1,9 @@
 class DashboardController < ApplicationController
   def index
-  	@temperatureDataset = [[18,17,18,19,21,22],[16,15,13,17,18,19],[6,5,3,7,8,10],[7,8,5,11,12,11],[17,12,5,10,15,11]]
+  	@temperatureDataset = [18,17,18,19,21,22]
+  	@humidityDataset = [55,55,55,55,55,55]
   	@dates = ["17/04/18", "18/04/18", "19/04/18", "20/04/18", "21/04/18", "22/04/18"]
-  	@roomNumber = 5
+  	@times = ["11:49:00", "11:49:00", "11:49:00", "11:49:00", "11:49:00", "11:49:00"]
+  	@roomNumber = 1
   end
 end
