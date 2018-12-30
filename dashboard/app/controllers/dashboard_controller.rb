c07b188822f1fb9f831d93255d1e997c4a1c6880
Calum <cjmcguicken@gmail.com>
2018-12-30 22:30:13 +0000

    Created dynamic data table

--- dashboard/app/controllers/dashboard_controller.rb
+++ dashboard/app/controllers/dashboard_controller.rb
@@ -5,5 +5,6 @@ class DashboardController < ApplicationController
   	@dates = ["17/04/18", "18/04/18", "19/04/18", "20/04/18", "21/04/18", "22/04/18"]
   	@times = ["11:49:00", "11:49:00", "11:49:00", "11:49:00", "11:49:00", "11:49:00"]
   	@roomNumber = 1
+  	@combinedDataset = @dates.zip(@times, @temperatureDataset, @humidityDataset)
   end
 end
