cc9a678fb8597fc667bb4ca7fac9fe2cd23ba3b0
Calum <cjmcguicken@gmail.com>
2018-12-30 18:33:08 +0000

    Chart JS files updated to accommodate humid and temp

--- dashboard/app/assets/javascripts/chart.js
+++ dashboard/app/assets/javascripts/chart.js
@@ -9,78 +9,26 @@ $(document).ready(function(){
 	grey: 'rgb(201, 203, 207)',
 	darkgrey: 'rgb(107, 107, 107)'
 	};
-	var roomtemp = $('#graphCanvas').data('dataset');
+	var roomtemp = $('#graphCanvas').data('temperatures');
+	var roomhumidity = $('#graphCanvas').data('humidities');
 	var dates = $('#graphCanvas').data('dates');
-	var numberOfRooms = $('#graphCanvas').data('roomNumber'); 
-	var room1 = roomtemp[0];
-	var room2 = roomtemp[1];
-	var room3 = roomtemp[2];
-	var room4 = roomtemp[3];
-	var room5 = roomtemp[4];
-	var roomtempAverage = [];
-	var count = 0;
-	for (i = 0; i < 6; i++) {
-	    	for (j = 0; j < 5; j++) {
-			    count += roomtemp[j][i];
-			    if (j == 4) {
-				    roomtempAverage[i] = (count/(j+1));
-				    count = 0;
-				}
-			}
-	}
+	//Possibility of adding multiple rooms here, insert loop to go through 2D array
+    //var room1 = roomtemp[0];
+	var numberOfRooms = $('#graphCanvas').data('roomNumber');
+	console.log($('#graphCanvas').data())
+
 	var ctx = document.getElementById("temperatureChart").getContext('2d');
 	var myChart = new Chart(ctx, {
 	    type: 'line',
 	    data: {
 	        labels: dates,
 	        datasets: [{
-	            label: 'Room 1',
-	            data: room1,
+	            label: 'Temperature',
+	            data: roomtemp,
 	            fill: false,
 				backgroundColor: window.chartColors.red,
 				borderColor: window.chartColors.red,
 	            borderWidth: 1
-	        },
-	        {
-				label: 'Room 2',
-			    data: room2,
-	            fill: false,
-				backgroundColor: window.chartColors.blue,
-				borderColor: window.chartColors.blue,
-	            borderWidth: 1        	
-	        },
-	        {
-				label: 'Room 3',
-			    data: room3,
-	            fill: false,
-				backgroundColor: window.chartColors.green,
-				borderColor: window.chartColors.green,
-	            borderWidth: 1  	        	
-	        },
-	        {
-				label: 'Room 4',
-			    data: room4,
-	            fill: false,
-				backgroundColor: window.chartColors.yellow,
-				borderColor: window.chartColors.yellow,
-	            borderWidth: 1 	        	
-	        },
-	        {
-				label: 'Room 5',
-			    data: room5,
-	            fill: false,
-				backgroundColor: window.chartColors.purple,
-				borderColor: window.chartColors.purple,
-	            borderWidth: 1 	        	
-	        },
-	        {
-				label: 'Average',
-			    data: roomtempAverage,
-	            fill: false,
-				backgroundColor: window.chartColors.darkgrey,
-				borderColor: window.chartColors.darkgrey,
-	            borderWidth: 1,
-	            borderDash: [10,5]	        	
 	        }]
 	    },
 	    options: {
