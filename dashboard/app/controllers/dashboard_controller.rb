0ce988574d25fa2260472498359dc0d836586798
Calum <cjmcguicken@gmail.com>
2018-12-31 14:09:34 +0000

    Added exporttocsv JS file

--- dashboard/app/controllers/dashboard_controller.rb
+++ dashboard/app/controllers/dashboard_controller.rb
@@ -3,7 +3,7 @@ class DashboardController < ApplicationController
   	@temperatureDataset = [18,17,18,19,21,22]
   	@humidityDataset = [55,55,55,55,55,55]
   	@dates = ["17/04/18", "18/04/18", "19/04/18", "20/04/18", "21/04/18", "22/04/18"]
-  	@times = ["11:49:00", "11:49:00", "11:49:00", "11:49:00", "11:49:00", "11:49:00"]
+  	@times = ["11:49:00", "10:49:00", "11:49:00", "23:49:00", "11:49:00", "01:49:00"]
   	@roomNumber = 1
   	@combinedDataset = @dates.zip(@times, @temperatureDataset, @humidityDataset)
   end
